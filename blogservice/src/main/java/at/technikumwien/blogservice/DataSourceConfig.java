6a8a5e3d4397a37306cda9a590d9e87342d9d251
unknown <rocky010mlg@gmail.com>
2020-12-26 22:53:30 +0100

    get datasource from config server in blogservice

---
+++ blogservice/src/main/java/at/technikumwien/blogservice/DataSourceConfig.java
@@ -0,0 +1,31 @@
+package at.technikumwien.blogservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${datasource.driverClassName}")
+    private String driverClassName;
+    @Value("${datasource.url}")
+    private String url;
+    @Value("${datasource.username}")
+    private String username;
+    @Value("${datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driverClassName);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+}
