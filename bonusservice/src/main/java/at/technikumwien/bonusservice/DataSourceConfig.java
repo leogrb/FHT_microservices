1f75031d051a47124f3ed9d67441becd73c33f94
Stefan Miljevic <stefi5miljevic@gmail.com>
2020-12-27 14:30:58 +0100

    remove not needed code

--- bonusservice/src/main/java/at/technikumwien/bonusservice/DataSourceConfig.java
+++ bonusservice/src/main/java/at/technikumwien/bonusservice/DataSourceConfig.java
@@ -3,7 +3,6 @@ package at.technikumwien.bonusservice;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class DataSourceConfig {
 
 	@Value("${datasource.driverClassName}")
-    private String driverClassName;
-    @Value("${datasource.url}")
-    private String url;
-    @Value("${datasource.username}")
-    private String username;
-    @Value("${datasource.password}")
-    private String password;
+	private String driverClassName;
+	@Value("${datasource.url}")
+	private String url;
+	@Value("${datasource.username}")
+	private String username;
+	@Value("${datasource.password}")
+	private String password;
 
 	@Bean
 	public DataSource dataSource() {
